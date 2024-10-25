a884a4b0945dd58ea5ccef5bacf3b89d40e635a6
Polina Prokopenko <topolina1337@gmail.com>
2024-10-26 00:22:39 +0200

    first commit

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls" },
+			})
+		end,
+	},
+	{
+		-- ":h vim.lsp.buf" for keybinding options
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
+
+			-- Keybindings
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
