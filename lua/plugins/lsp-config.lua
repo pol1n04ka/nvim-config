3eedf93027e7134d801c8920df0007166f1e4603
Polina Prokopenko <topolina1337@gmail.com>
2024-12-25 21:07:05 +0100

    added autoompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,14 +17,19 @@ return {
 		-- ":h vim.lsp.buf" for keybinding options
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
+
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.ts_ls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
 
 			-- Keybindings
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 		end,
 	},
 }
