7868cf9ed43e034639d98db6caf629f8fc3a4040
Polina Prokopenko <topolina1337@gmail.com>
2024-12-27 23:29:10 +0100

    added nvim-surround and changed some vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,20 @@
-vim.cmd("set number")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.o.relativenumber = true
+
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.o.wrap = false
+
+vim.o.termguicolors = true
+
+vim.o.scrolloff = 8
+
+vim.o.updatetime = 50
+
+vim.o.colorcolumn = "80"
+
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>w", ":w<CR>", {})
