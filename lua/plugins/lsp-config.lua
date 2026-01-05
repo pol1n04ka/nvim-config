edd0639f6afa022f1f06d41cb8bce10c9174422d
Polina Prokopenko <topolina1337@icloud.com>
2026-01-05 15:47:54 +0100

    sync with pc

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,17 +19,21 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+			-- Use vim.lsp.config instead of require('lspconfig')
+			vim.lsp.config('lua_ls', { capabilities = capabilities })
+			vim.lsp.config('ts_ls', { capabilities = capabilities })
+			vim.lsp.config('clangd', { capabilities = capabilities })
 
-			lspconfig.lua_ls.setup({ capabilities = capabilities })
-			lspconfig.ts_ls.setup({ capabilities = capabilities })
-			lspconfig.clangd.setup({ capabilities = capabilities })
+			-- Enable the servers
+			vim.lsp.enable('lua_ls')
+			vim.lsp.enable('ts_ls')
+			vim.lsp.enable('clangd')
 
 			-- Keybindings
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-		end,
+    end,
 	},
 }
