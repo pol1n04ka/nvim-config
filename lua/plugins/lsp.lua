e05e7e45b622ffede7d5740c19a63150d195a060
Polina Prokopenko <topolina1337@icloud.com>
2026-06-06 22:05:58 +0200

    fix: identation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,8 @@
 vim.pack.add {
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-  { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 }
 
 require("mason").setup()
